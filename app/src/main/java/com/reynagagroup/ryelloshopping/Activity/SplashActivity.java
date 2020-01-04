beab8f83ef3ad0949d72ea7ee6762f8a2e0431da
dannub <fardannubimantara@gmail.com>
2020-01-04 22:38:40 +0700

    mengupdate user last seen

--- app/src/main/java/com/reynagagroup/ryelloshopping/Activity/SplashActivity.java
+++ app/src/main/java/com/reynagagroup/ryelloshopping/Activity/SplashActivity.java
@@ -1,13 +1,19 @@
 package com.reynagagroup.ryelloshopping.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.reynagagroup.ryelloshopping.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -40,11 +46,29 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-
-                    Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
-                    MainActivity.showCart =false;
+                if (currentUser!=null){
+                    FirebaseFirestore.getInstance().collection("USERS").document(currentUser.getUid()).update("Last seen", FieldValue.serverTimestamp())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                                        MainActivity.showCart =false;
+                                        startActivity(mainIntent);
+                                        finish();
+                                    }else {
+                                        String error = task.getException().getMessage();
+                                        Toast.makeText(SplashActivity.this,error,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }else {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    MainActivity.showCart = false;
                     startActivity(mainIntent);
                     finish();
+                }
+
             }
             public  void  finish(){
                 SplashActivity.this.finish();
